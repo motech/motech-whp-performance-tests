0d005e935b6527ede5ca41bfb05122a961a4a3d6
Vishal Naik <vishalnaik@thoughtworks.com>
2012-12-24 12:36:44 +0530

    Vishal #0 Parameterizing provider reminder test for incrementally increasing number of providers.

--- src/test/java/org/motechproject/whp/performancetests/providerreminder/ReminderEventHandlerPerformanceIT.java
+++ src/test/java/org/motechproject/whp/performancetests/providerreminder/ReminderEventHandlerPerformanceIT.java
@@ -1,30 +1,47 @@
-package org.motechproject.whp.providerreminder;
+package org.motechproject.whp.performancetests.providerreminder;
 
 import org.apache.log4j.Logger;
 import org.databene.benerator.main.Benerator;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.kubek2k.springockito.annotations.ReplaceWithMock;
 import org.kubek2k.springockito.annotations.SpringockitoContextLoader;
 import org.mockito.ArgumentCaptor;
 import org.motechproject.event.MotechEvent;
 import org.motechproject.http.client.service.HttpClientService;
 import org.motechproject.whp.common.service.IvrConfiguration;
+import org.motechproject.whp.providerreminder.service.ProviderReminderService;
 import org.motechproject.whp.providerreminder.service.ReminderEventHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.TestContextManager;
 import org.springframework.util.StopWatch;
 
 import java.io.IOException;
+import java.util.Collection;
 
+import static java.util.Arrays.asList;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.motechproject.whp.common.event.EventKeys.ADHERENCE_WINDOW_APPROACHING_EVENT_NAME;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(Parameterized.class)
 @ContextConfiguration(loader = SpringockitoContextLoader.class, locations ={"classpath*:/applicationProviderReminderContext.xml", "classpath:applicationContextPerformanceIT.xml"})
 public class ReminderEventHandlerPerformanceIT {
+
+    private int numberOfProvidersToAdd;
+    private int numberOfProviders;
+
+    private TestContextManager testContextManager;
+
+
+    public ReminderEventHandlerPerformanceIT(int numberOfProvidersToAdd, int numberOfProviders) {
+        this.numberOfProvidersToAdd = numberOfProvidersToAdd;
+        this.numberOfProviders = numberOfProviders;
+    }
 
     @ReplaceWithMock
     @Autowired
@@ -32,23 +49,39 @@ public class ReminderEventHandlerPerformanceIT {
 
     @Autowired
     ReminderEventHandler reminderEventHandler;
+
+    @Autowired
+    ProviderReminderService providerReminderService;
+
     @Autowired
     IvrConfiguration ivrConfiguration;
 
+    @Before
+    public void setUp() throws Exception {
+        testContextManager = new TestContextManager(getClass());
+        testContextManager.prepareTestInstance(this);
+    }
+
     private Logger logger = Logger.getLogger(this.getClass().getName());
 
-    @Test
-    public void shouldSendReminderFor1000Providers() throws IOException {
-        Benerator.main(new String[]{"createProvidersAndPatients.ben.xml"});
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] { { 10, 10 }, { 100, 100 }, { 500, 500 }, { 500, 1000 } };
+        return asList(data);
+    }
 
-        StopWatch stopWatch = new StopWatch("reminder-1000-providers");
+    @Test
+    public void shouldSendReminderForProviders() throws IOException {
+        Benerator.main(new String[]{String.format("createProvidersAndPatients.%s.ben.xml", numberOfProvidersToAdd)});
+
+//        providerReminderService.getActiveProviderPhoneNumbers(); //prime the couchdb indexes
+
+        StopWatch stopWatch = new StopWatch(String.format("reminder-%s-providers", numberOfProviders));
         stopWatch.start();
 
         reminderEventHandler.adherenceWindowApproachingEvent(new MotechEvent(ADHERENCE_WINDOW_APPROACHING_EVENT_NAME));
 
         stopWatch.stop();
-
-        System.out.println(stopWatch.shortSummary());
 
         ArgumentCaptor<String> reminderXmlCaptor = ArgumentCaptor.forClass(String.class);
         verify(httpClientService).post(eq(ivrConfiguration.getProviderReminderUrl()), reminderXmlCaptor.capture());
@@ -56,5 +89,7 @@ public class ReminderEventHandlerPerformanceIT {
 
         logger.debug("Reminder Request: " + reminderXml);
         logger.info(stopWatch.shortSummary());
+
+        reset(httpClientService);
     }
 }
