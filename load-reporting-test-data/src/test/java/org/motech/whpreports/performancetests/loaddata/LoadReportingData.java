eae1fb22c5a7db54e821529fa799aedd4f81b885
Abhishek <asharma@thoughtworks.com>
2013-04-30 18:06:04 +0530

    Abhi #0 Adding scripts to load reporting data

--- load-reporting-test-data/src/test/java/org/motech/whpreports/performancetests/loaddata/LoadReportingData.java
+++ load-reporting-test-data/src/test/java/org/motech/whpreports/performancetests/loaddata/LoadReportingData.java
@@ -10,20 +10,44 @@ public class LoadReportingData {
     @Test
     public void shouldLoadPatientsAndProviders() throws IOException {
         try{
-            System.setProperty("count", String.valueOf(100));
-            Benerator.main(new String[]{"createDataForReporting.ben.xml"});
+            System.setProperty("provider_count", String.valueOf(10000));
+            System.setProperty("patient_count", String.valueOf(10));
+            Benerator.main(new String[]{"createPatientProviderDataForReporting.ben.xml"});
         }catch (Exception exception){
-
+            exception.printStackTrace();
         };
     }
 
     @Test
-    public void shouldLoadAdherenceAuditLogs() throws IOException {
+    public void shouldLoadAdherenceData() throws IOException {
         try{
-            System.setProperty("count", String.valueOf(100));
-            Benerator.main(new String[]{"createAdherenceAuditLogsForReporting.ben.xml"});
+            System.setProperty("adherence_audit_log_count", String.valueOf(10));
+            System.setProperty("adherence_call_log_count", String.valueOf(10));
+            Benerator.main(new String[]{"createAdherenceDataForReporting.ben.xml"});
         }catch (Exception exception){
+            exception.printStackTrace();
+        };
+    }
 
+    @Test
+    public void shouldLoadContainerData() {
+        try{
+            System.setProperty("container_count", String.valueOf(100000));
+            System.setProperty("container_call_log_count", String.valueOf(100000));
+            Benerator.main(new String[]{"createContainerDataForReporting.ben.xml"});
+        }catch (Exception exception){
+            exception.printStackTrace();
+        };
+    }
+
+    @Test
+    public void shouldLoadReminderDataForProviderAndPatients() {
+        try{
+            System.setProperty("provider_reminder_call_log_count", String.valueOf(100000));
+            System.setProperty("patient_alert_count", String.valueOf(100000));
+            Benerator.main(new String[]{"createRemindersDataForReporting.ben.xml"});
+        }catch (Exception exception){
+            exception.printStackTrace();
         };
     }
 
